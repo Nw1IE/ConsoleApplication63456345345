a0d1f81437a34881bf9479975b85de5e392dff68
Admin <Admin@DESKTOP-E7OIGPE>
2024-10-09 15:10:03 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication63456345345/ConsoleApplication63456345345.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cstdlib> 
+
+
+void subtract(int a, int b) {
+    int result = a - b;
+    std::cout << "Ответ вычитания (" << a << " - " << b << "): " << result << std::endl;
+}
+
+
+void multiply(int a, int b) {
+    int result = a * b;
+    std::cout << "Ответ умножения (" << a << " * " << b << "): " << result << std::endl;
+}
+
+
+void divide(int a, int b) {
+    if (b == 0) {
+        std::cout << "Ошибка: Деление на ноль низя" << std::endl;
+        exit(EXIT_FAILURE); 
+    }
+    double result = static_cast<double>(a) / b;
+    std::cout << "Ответ Деления (" << a << " / " << b << "): " << result << std::endl;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+
+    int num1, num2;
+    char operation;
+
+    std::cout << "Первое число: ";
+    std::cin >> num1;
+
+    std::cout << "(-, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Второе число: ";
+    std::cin >> num2;
+
+    switch (operation) {
+    case '-':
+        subtract(num1, num2);
+        break;
+    case '*':
+        multiply(num1, num2);
+        break;
+    case '/':
+        divide(num1, num2);
+        break;
+    default:
+        std::cout << "Ошибка: Неверная операция." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
